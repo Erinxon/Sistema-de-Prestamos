6a622905948ea05faee6b76faa6885ffedceebba
Erinxon <30541728+Erinxon@users.noreply.github.com>
2022-01-04 15:36:18 -0400

    Commit

--- Prestamos.Client/Prestamos/src/app/modules/admin/admin.component.ts
+++ Prestamos.Client/Prestamos/src/app/modules/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Pagina } from 'src/app/Core/models/paginas/paginas.model';
 import { UserAuth } from 'src/app/Core/models/userAuth.model';
@@ -23,8 +24,11 @@ export class AdminComponent implements OnInit {
   ];
   titulo:string = 'Prestamos';
   user!: UserAuth;
+
+  sidebarMini: boolean = false;
   constructor(private auth: AuthService,
-    private router: Router) { 
+    private router: Router,
+    @Inject (DOCUMENT) private document: Document) { 
       this.auth.getUserAuth().subscribe(res => {
         this.user = res;
       });
@@ -45,6 +49,38 @@ export class AdminComponent implements OnInit {
   getLetrasIniciales(nombres: string, apellidos: string): string {
     return  nombres.charAt(0) + apellidos.charAt(0);
   }
+
+  ocultarOMostrarMenu(){
+    this.sidebarMini = !this.sidebarMini;
+
+    if(this.contains('sidebar-gone')){
+     // this.document.body.classList.remove('sidebar-gone');
+      //this.document.body.classList.add('sidebar-show');
+      this.deleteClass('sidebar-gone');
+      this.addClass('sidebar-show');
+    }else{
+      if(this.sidebarMini){
+       // this.document.body.classList.add('sidebar-mini');
+        this.addClass('sidebar-mini');
+      }else{
+        //this.document.body.classList.remove('sidebar-mini');
+        this.deleteClass('sidebar-mini');
+      }
+    }
+  }
+
+  private contains(classCSS: string){
+    return this.document.body.classList.contains(classCSS);
+  }
+
+  private addClass(classCSS: string){
+    this.document.body.classList.add(classCSS);
+  }
+
+  private deleteClass(classCSS: string){
+    this.document.body.classList.remove(classCSS);
+  }
+
 
 
 }
