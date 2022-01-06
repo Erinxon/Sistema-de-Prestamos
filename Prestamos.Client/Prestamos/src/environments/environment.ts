83863d37c4146d13e6db3d4b025293ffe39ba565
Erinxon <30541728+Erinxon@users.noreply.github.com>
2022-01-06 14:20:30 -0400

    Add Fonts

--- Prestamos.Client/Prestamos/src/environments/environment.ts
+++ Prestamos.Client/Prestamos/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apikey: "2T8vA6Ni8WVGHna+yVMcSoefaJt4QpXkJvOqnEQO2KDGGj5QaH/nEbb0kNlNkoBdBO2SLn+eUv1eDvZ9YXOF5N1x+k+P6AjPkRE9azjejF4sg6pl73TwM8/W6T+ERvAeVbMJmgiYyOeFSFDXbTFQShdbUR7D/amOMTaJzF85N9rR1CYL7cAQbO0azVb9aYnVZOuID9g/82ip5kk0ImIe4dtoKMp3AOO7mBDkJ6NGpFg",
-  apiUrl: 'https://api-prestamos-money.herokuapp.com/api'
+  apiUrl: 'https://localhost:5001/api'
 };
 
 /*
